e92dd130a65dfecfdbcff2ac22ea668823305174
Osanda Muthukumarana <it22170798@my.sliit.lk>
2026-01-21 23:48:48 +0530

    did some changes to the other two sections   -- commit #42

--- frontend/src/components/Pages/Home/CompanyProfiles.tsx
+++ frontend/src/components/Pages/Home/CompanyProfiles.tsx
@@ -11,7 +11,7 @@ import Waves from '../../Ui/Waves';
 import CardSwap, { Card } from '../../Ui/CardSwapI';
 
 
-//datasets
+
 //data sets
 const serviceDetails = [
   {
@@ -189,7 +189,8 @@ export default function CompanyProfiles() {
 
           </div>
 
-
+          {/* Below Line Section */}
+          <div className="absolute z-50  bottom-0 w-full h-2 bg-gradient-to-l from-blue-500 via-blue-400/50 to-transparent shadow-[0_0_15px_rgba(59,130,246,0.9)]" />
 
 
         </div>
