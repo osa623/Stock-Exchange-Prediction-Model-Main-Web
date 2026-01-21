a5b03b92e21fc7c3c05de0d9eb9b863a7a9ed907
Osanda Muthukumarana <it22170798@my.sliit.lk>
2026-01-22 01:33:06 +0530

    did some changes to the portfolio section   -- commit #42

--- frontend/src/app/portfolio/page.tsx
+++ frontend/src/app/portfolio/page.tsx
@@ -5,7 +5,7 @@ import Portfolio from "@/components/Pages/Portfolio/page";
 
 export default function DashboardPage() {
   return (
-    <main className="min-h-screen text-white">
+    <main className="min-h-screen  bg-gradient-to-br from-[#0A0E1A] via-[#0D1425] to-[#182039]  text-white">
 
       <div className="container mx-auto px-6 py-8 space-y-8">
 
