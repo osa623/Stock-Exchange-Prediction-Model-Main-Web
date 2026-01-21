fe778497ebfac8b5f390f5ad25f10eda680f1748
Osanda Muthukumarana <it22170798@my.sliit.lk>
2026-01-21 23:22:41 +0530

    did some changes to themiddle section  -- commit #41

--- frontend/src/components/Pages/Home/MiddleSection1.tsx
+++ frontend/src/components/Pages/Home/MiddleSection1.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const MiddleSection1 = () => {
     return (
-        <div className="relative h-[20vh] w-full bg-white">
+        <div className="relative h-[40vh] w-full bg-white">
+
+
+            <div className="absolute z-40  bottom-0 w-full h-2 bg-gradient-to-r from-blue-500 via-blue-400/50 to-transparent shadow-[0_0_15px_rgba(59,130,246,0.9)]" />
 
         </div>
     )
